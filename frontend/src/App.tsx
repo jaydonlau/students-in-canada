c76eea07fd8b1cebb0c990c9fe065f2ba65b0390
Jaydon <lau.jaydon@gmail.com>
2020-02-08 14:38:22 -0500

    feat: add react router and home/login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,37 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Normalize } from 'styled-normalize';
-import logo from './logo.svg';
-import './App.css';
+
+import HomePage from 'HomePage';
+import LoginPage from 'LoginPage';
 
 const App: React.FC = () => {
   return (
-    <Fragment>
+    <Router>
       <Normalize />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload. 
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
-    </Fragment>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        /* make sure this is last route */
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

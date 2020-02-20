7d82ea34d780ebe1ad92e27e927af1810bad1b61
Jaydon <lau.jaydon@gmail.com>
2020-02-20 00:09:46 -0500

    whitespace

--- backend/sic_project/django_project/urls.py
+++ backend/sic_project/django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
 	path('users/', include('apps.auth.users.urls')),
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
 ]
